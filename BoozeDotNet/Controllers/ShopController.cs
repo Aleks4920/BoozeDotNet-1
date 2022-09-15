285cbca955a1f4589eb72dee634a774f44ac196a
ifotn <rich@infrontofthenet.com>
2022-09-15 10:15:05 -0400

    Add project files.

---
+++ BoozeDotNet/Controllers/ShopController.cs
@@ -0,0 +1,34 @@
+﻿using BoozeDotNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoozeDotNet.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Category(string CategoryName)
+        {
+            if (CategoryName == null)
+            {
+                return RedirectToAction("Index");
+            }
+            
+            // pass input param val to ViewData for display in the view
+            ViewData["CategoryName"] = CategoryName;
+
+            // use Product model to mock a list of products for display
+            var products = new List<Product>();
+
+            for (var i = 1; i < 11; i++)
+            {
+                products.Add(new Product { ProductId = i, Name = "Product " + i.ToString() });
+            }
+
+            return View(products);
+        }
+    }
+}
---
+++ BoozeDotNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BoozeDotNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
